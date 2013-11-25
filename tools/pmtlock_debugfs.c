d1e6a02314b14c7588795e81d439fa083249da85
Jiannan Ouyang <ouyang@cs.pitt.edu>
2013-11-25 15:40:06 -0500

    add lhp_lwp_perf.patch and debugfs support

--- tools/pmtlock_debugfs.c
+++ tools/pmtlock_debugfs.c
@@ -33,7 +33,13 @@ int pmtlock_init_debugfs(void)
         return -1;
     }
 
+    /* Add kernel variable you want to inspect here */
     debugfs_create_u64("pmtlock_timeout_shift", 0644, d_pmtlock_debugfs, &pmtlock_timeout_shift);
+    /*
+    debugfs_create_u64("LHP", 0644, d_pmtlock_debugfs, &LHP);
+    debugfs_create_u64("LWP", 0644, d_pmtlock_debugfs, &LWP);
+    debugfs_create_u64("waiting_threshold", 0644, d_pmtlock_debugfs, &waiting_threshold);
+    */
 
     return 0;
 }
